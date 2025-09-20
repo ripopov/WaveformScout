34850831453a6e7ebbd431caba944881957b430d
Roman Popov <ripopov@gmail.com>
2025-09-19 19:54:19 -0700

    feat: Introduce type stubs and finalize pyrox module structure
    - Added `py.typed` for type hint support.
    - Implemented type stubs (`pyrox.pyi`) to define public API types.
    - Updated `pyproject.toml` to specify version and Python package metadata.
    - Enhanced `__init__.py` with `__all__` for public API exports.

--- pyrox/pyrox/__init__.py
+++ pyrox/pyrox/__init__.py
@@ -1,1 +1,17 @@
 from pyrox.pyrox import *
+
+__all__ = [
+    "Hierarchy",
+    "Scope",
+    "ScopeIter",
+    "Signal",
+    "SignalChangeIter",
+    "QueryResult",
+    "TimeTable",
+    "Timescale",
+    "TimescaleUnit",
+    "Var",
+    "VarIndex",
+    "VarIter",
+    "Waveform",
+]
