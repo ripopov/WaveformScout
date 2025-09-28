dd6dedf4849eaade8a9f1ffca4af14e1aecfac31
Roman Popov <ripopov@gmail.com>
2025-09-27 17:23:39 -0700

    test: Pass `db` parameter to `create_signal_node_from_var` in ROI zoom test

--- tests/test_roi_zoom.py
+++ tests/test_roi_zoom.py
@@ -65,7 +65,7 @@ class ROITestHelper:
                 if signal_count >= 3:  # Add 3 signals for testing
                     break
                 full_name = var.full_name(db.hierarchy)
-                node = create_signal_node_from_var(var, db.hierarchy, handle)
+                node = create_signal_node_from_var(var, db.hierarchy, handle, db)
                 node.name = full_name
                 session.root_nodes.append(node)
                 signal_count += 1
